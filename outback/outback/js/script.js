c717bd28744f3b24252def4d54c318539fe28323
michelsmartins-s <2025311597@aluno.riogrande.ifrs.edu.br>
2026-03-28 21:20:30 -0300

    Delete outback/outback directory

--- outback/outback/js/script.js
+++
@@ -1,6 +0,0 @@
-const bnt = document.querySelector('.menu-nav');
-const links = document.querySelector('.main-links');
-
-bnt.addEventListener ('click', function() {
-    links.classList.toggle('aberta');
-});
